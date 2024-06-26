54edd44bf0b5383b016e23a49a6242a28ada0d0c
Timothy Babatunde Akanbi <knbtimothy@gmail.com>
2024-06-26 20:30:24 +0100

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,52 @@
 import H2 from "@/components/H2";
 import RedirectButton from "@/components/RedirectButton";
-import { Facebook, Instagram, Twitter } from "@/components/Svgs";
-import { Whatsapp } from "../components/Svgs";
+import { Facebook, Instagram, SnapChatIcon, TiktokIcon, Twitter } from "@/components/Svgs";
 
+export async function generateMetadata() {
+  const title = 'Viral';
+  const image = '';
+  const description = '';
+  const url = '';
+  return {
+    title: title,
+    description: description,
+    category: "Website",
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      siteId: "",
+      creator: "@timothy_akanbii",
+      creatorId: "",
+      images: []
+    },
+    openGraph: {
+      title,
+      description,
+      url,
+      siteName: "Viral Website",
+      images: [
+        {
+          url: image,
+          width: 800,
+          height: 800
+        },
+      ],
+    },
+    locale: "en_US",
+    type: "website"
+  }
+}
 
 export default function Home() {
+
+  const facebookURL  : string = 'https://www.facebook.com/share/Ts4ACAKDaqJYuSkr/?mibextid=LQQJ4d';
+  const instagramURL : string = 'https://www.instagram.com/invites/contact/?i=16m2ifiy3pxee&utm_content=igsk3uz';
+  const snapChatURL : string = 'https://www.snapchat.com/add/ayanfevira?share_id=1193ulqzQsuLKuj5nYHFdA&locale=en_US';
+  const TiktokURL : string = 'https://www.tiktok.com/@ayanfevira?_t=8mt04KCGBL3&_r=1';
+
+  
+
   return (
     <main className="">
       <H2
@@ -16,25 +58,26 @@ export default function Home() {
       <div className="flex justify-center">
       <div className="mt-[20px]  grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-3">
           <RedirectButton 
-            href={"/"}
+            href={facebookURL}
             text="Facebook"
             children={<Facebook />}
           />
           <RedirectButton 
-            href={"/"}
+            href={instagramURL}
             text="Instagram"
             children={<Instagram />}
           />
           <RedirectButton 
-            href={"/"}
-            text="Twitter"
-            children={<Twitter />}
+            href={snapChatURL}
+            text="Snapchat"
+            children={<SnapChatIcon />}
           />
           <RedirectButton 
-            href={"https://wa.me/+2348125775404"}
-            text="WhatsApp"
-            children={<Whatsapp />}
+            href={TiktokURL}
+            text="Tiktok"
+            children={<TiktokIcon />}
           />
+
         </div>
      </div>
     </main>
--- src/components/RedirectButton.tsx
+++ src/components/RedirectButton.tsx
@@ -12,6 +12,7 @@ const RedirectButton = ({ href, text, children }: RedirectButtonType) => {
     <Link 
         className="bg-[#565555] py-[8px] px-[12px] rounded-[16px] flex justify-center items-center"
         href={href}
+        target='_blank'
     >
         {children}
         <div className="ml-[12px] text-fourteen font-fourHundrend text-primary">{text}</div>
